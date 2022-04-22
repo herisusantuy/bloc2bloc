f7d0e8dd068aa99a71b43f9326caf0e47d120628
askloraheri <heri.susanto@loratechai.com>
2022-04-23 00:11:50 +0700

    Change to Bloc listener

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,7 @@ class MyApp extends StatelessWidget {
           create: (context) => ColorBloc(),
         ),
         BlocProvider<CounterBloc>(
-          create: (context) => CounterBloc(
-            colorBloc: context.read<ColorBloc>(),
-          ),
+          create: (context) => CounterBloc(),
         )
       ],
       child: MaterialApp(
@@ -45,56 +43,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  int incrementSize = 1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.watch<ColorBloc>().state.color,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                context.read<ColorBloc>().add(ChangeColorEvent());
-              },
-              child: const Text(
-                'Change color',
-                style: TextStyle(fontSize: 24.0),
+    return BlocListener<ColorBloc, ColorState>(
+      listener: (context, colorState) {
+        if (colorState.color == Colors.red) {
+          incrementSize = 1;
+        } else if (colorState.color == Colors.green) {
+          incrementSize = 10;
+        } else if (colorState.color == Colors.blue) {
+          incrementSize = 100;
+        } else if (colorState.color == Colors.black) {
+          incrementSize = -100;
+          context
+              .read<CounterBloc>()
+              .add(ChangeCounterEvent(incrementSize: incrementSize));
+        }
+      },
+      child: Scaffold(
+        backgroundColor: context.watch<ColorBloc>().state.color,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  context.read<ColorBloc>().add(ChangeColorEvent());
+                },
+                child: const Text(
+                  'Change color',
+                  style: TextStyle(fontSize: 24.0),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              '${context.watch<CounterBloc>().state.counter}',
-              style: const TextStyle(
-                  fontSize: 52.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.read<CounterBloc>().add(ChangeCounterEvent());
-              },
-              child: const Text(
-                'Increment counter',
-                style: TextStyle(fontSize: 24.0),
+              const SizedBox(
+                height: 20.0,
               ),
-            )
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              Text(
+                '${context.watch<CounterBloc>().state.counter}',
+                style: const TextStyle(
+                    fontSize: 52.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  context
+                      .read<CounterBloc>()
+                      .add(ChangeCounterEvent(incrementSize: incrementSize));
+                },
+                child: const Text(
+                  'Increment counter',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              )
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
